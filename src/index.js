7a414486d6699ce0d4cc6825d32cc3a868131ce9
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-02 14:47:38 -0500

    Initiate commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div className="MainApp">
+    <App />
+  </div>
+, document.getElementById('root'));
+
